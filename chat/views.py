713e303598cbdac14b8de36d0dec25a65f835b09
Devyanshi Srivastava <devyanshi2106@gmail.com>
2023-07-20 23:38:16 +0530

    Signup

--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import SignUpForm
 
 def index(request):
     return render(request, 'chat/index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+
+            login(request, user)
+
+            return redirect('index')
+
+    else:
+        form = SignUpForm()
+
+    return render(request, 'chat/signup.html', {
+        'form' : form
+    })
